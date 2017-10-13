663267e9d6cdf4fc880cccf812e99c18bdd27eee
Tatuya Kamada <kmdtty@users.noreply.github.com>
2017-10-14 02:16:28 +0900

    Modify the interface.

--- normal.go
+++ normal.go
@@ -1,8 +1,41 @@
 package normal
 
-func merge_s(x string, y string) string {
-  return x + y
+import (
+  "fmt"
+  "encoding/json"
+
+  "github.com/koron/go-dproxy"
+)
+
+type JSON = dproxy.Proxy
+
+func merge(x string, diff string) string {
+  return x + diff
 }
-func diff() string {
-  return "Normal"
+
+/*                
+ JavaScript Object Notation (JSON) Patch
+ https://tools.ietf.org/html/rfc6902
+
+ JavaScript Object Notation (JSON) Pointer
+ https://tools.ietf.org/html/rfc6901
+ 
+ The application/json Media Type for JavaScript Object Notation (JSON)
+ https://tools.ietf.org/html/rfc4627
+ */
+func diff(x string, y string) string {
+  var x_record interface{}
+  var y_record interface{}
+  json.Unmarshal([]byte(x), &x_record)
+  json.Unmarshal([]byte(y), &y_record)
+  x_json := dproxy.New(x_record)
+  y_json := dproxy.New(y_record)
+  fmt.Sprintf("%q", x_json)
+  if x_json == y_json {
+    return ""
+  }
+  return `[{"op":"replace", path:"/a", "value": 3},
+           {"op":"remove",  path:"/b"},
+           {"op":"add", path:"/c", "value": 4}]
+          `
 }
--- normal_test.go
+++ normal_test.go
@@ -4,9 +4,15 @@ import (
   "testing"
 )
 
-func TestNormal(t *testing.T) {
-  result := merge_s("a", "b")
-  expected := "abc"
+func Test_diff(t *testing.T) {
+  d := diff(`{"a":"1", "b":"2"}`, `{"a":"3", "c":"4"}`)
+  t.Logf("%v", d)
+}
+
+func _Test_merge(t *testing.T) {
+  d := diff(`{"a":1, "b":2}`, `{"a":3, "c":4}`)
+  result := merge(`{"a":1, "b":2}`, d)
+  expected := `{"a":3, "c":4}`
   if result != expected {
     t.Errorf("The merge result: %q is not expecting word: %q", result, expected) 
   }
